635b2e25f3bc99c5eaec3829df75e4c705cac414
Jenorg <jenfermz44@gmail.com>
2025-06-15 20:11:45 -0500

    fix: :bug: rquire bug fixed

--- src/components/layout/aside/Aside.tsx
+++ src/components/layout/aside/Aside.tsx
@@ -50,19 +50,19 @@ const Aside = () => {
   );
 }
 
-function MenuItem({ icon, text }: { icon: string; text: string }) {
-  const IconComponent = require(`lucide-react`)[icon];
-  return (
-    <li>
-      <a
-        href="#"
-        className="flex items-center px-4 py-2 hover:bg-gray-800 rounded transition-colors gap-3"
-      >
-        {IconComponent && <IconComponent className="w-5 h-5" />}
-        <span>{text}</span>
-      </a>
-    </li>
-  );
-}
+// function MenuItem({ icon, text }: { icon: string; text: string }) {
+//   const IconComponent = require(`lucide-react`)[icon];
+//   return (
+//     <li>
+//       <a
+//         href="#"
+//         className="flex items-center px-4 py-2 hover:bg-gray-800 rounded transition-colors gap-3"
+//       >
+//         {IconComponent && <IconComponent className="w-5 h-5" />}
+//         <span>{text}</span>
+//       </a>
+//     </li>
+//   );
+// }
 
 export default Aside;
