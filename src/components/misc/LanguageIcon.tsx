444d75804b034e398a27dba2b6f5b52ced10430d
tovar21 <tovarfigueroa@gmail.com>
2025-07-20 14:02:29 -0400

    Arreglado icono node

--- src/components/misc/LanguageIcon.tsx
+++ src/components/misc/LanguageIcon.tsx
@@ -10,11 +10,14 @@ export function LanguageIcon({
   languageIcon,
   languageName,
 }: LanguageIconProps) {
+  // Si el icono es 'node.png', usar 'nodejs.png' en su lugar
+  let iconFile = languageIcon;
+  if (iconFile === 'node.png') iconFile = 'nodejs.png';
   return (
     <>
       <div className="flex justify-center items-center gap-2">
         <Avatar className=" sm:w-5 sm:h-5 h-4 w-4">
-          <AvatarImage src={`/pngs/${languageIcon}`} alt={"icon-img"} />
+          <AvatarImage src={`/pngs/${iconFile}`} alt={"icon-img"} />
         </Avatar>
         <p className="text-xs sm:text-sm">{languageName}</p>
       </div>
